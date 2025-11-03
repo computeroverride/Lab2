9b44c6fefaaedf74f9b7dc613a2d2e9a7e4f866f
Nethra <nethravino77@gmail.com>
2025-11-03 14:47:56 +0800

    Refactor bmi function: ensure return values for weight categories

--- bmi.py
+++ bmi.py
@@ -5,11 +5,14 @@ def bmi(height, weight):
     if bmi>25.0:
         print(bmi)
         print("overweight")
+        return 1
     elif bmi>=18.5:
         print(bmi)
         print("Normal weight")
+        return 0
     else: 
         print(bmi)
         print("underweight")
+        return -1
 
 bmi(height=1.7, weight=73)
