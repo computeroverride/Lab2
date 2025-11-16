8a6337a89bfdb97a68a7d69b42ec315a7b0bbd9e
Nethra <nethravino77@gmail.com>
2025-11-16 18:29:43 +0800

    Update Lab2 for BMI tests

--- bmi.py
+++ bmi.py
@@ -1,18 +1,10 @@
-def bmi(height, weight):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-    bmi=weight/(height**2)
-    if bmi>25.0:
-        print(bmi)
-        print("overweight")
+def calculate_bmi(height, weight):
+    bmi_value = weight / (height ** 2)
+    if bmi_value > 25.0:
         return 1
-    elif bmi>=18.5:
-        print(bmi)
-        print("Normal weight")
+    elif bmi_value >= 18.5:
         return 0
-    else: 
-        print(bmi)
-        print("underweight")
+    else:
         return -1
-
-bmi(height=1.7, weight=73)
+def bmi(height, weight):
+    return calculate_bmi(height, weight)
